fa3d9f40a4173c4c7d1da0ed22556f1a798f2fc6
Destiny-Ed <dikeachaeze@gmail.com>
2021-02-26 06:52:41 +0100

    Added print to output all odd numbers before printing their sum

--- group4.py
+++ group4.py
@@ -7,16 +7,19 @@
 #Psuedo Code 
 #........................................................................
 
+#1#Declare a function
+#2#Wrap all statement in the function inside try...except to handle any form of exception
 
-#1# Input a number between 1 and 100 inclusive from a user and save as maximunNumb
-#2# Declare a varible oddSum to store the total sum and initialize it to 0
-#3# If maximumNumb is not a valid number. then restart program .............
-#4# While maximumNumb is less than 1 or greater than 100 ask user to input a number
+#3# Input a number between 1 and 100 inclusive from a user and save as maximunNumb
+#4# If maximumNumb is not a valid number. then restart program .............
 
-#5# Iterate through the maximumNumb by checking for the difference with 1
-#6# Use control structure to check whether the difference is an odd numbers
-#7# Add the oddSum with the odd numbers from the control structure
-#8# Output the oddSum
+#5# Declare a variable oddSum to store the total sum and first initialize it to 0
+#6# While maximumNumb is less than 1 or greater than 100 ask user to input a number again
+
+#7# Iterate through the maximumNumb by checking for the difference with 1
+#8# Use control structure to check whether the difference is an odd numbers
+#9# Compute the odd numbers and store then to the oddsum
+#10# Output the oddSum which now contains the sum of all numbers
 
 
 
@@ -31,6 +34,7 @@ def callFunction():
 
         for numbers in range(1, maximumNumb + 1):
             if(numbers % 2 == 1):
+                print("Odd number " + str(numbers))
                 oddSum = oddSum + numbers
                 
         print("The sum of the odd numbers : " + str(oddSum) )
